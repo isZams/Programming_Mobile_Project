db8bd5e9ba25c66df21b6859d34074830e6bd9de
rickardomc <salvatelliriccardo@gmail.com>
2021-05-06 18:11:12 +0200

    add signUpWithEmail

---
+++ app/src/main/java/com/example/programming_mobile_project/LoginFragment/AuthRepo.kt
@@ -0,0 +1,36 @@
+package com.example.programming_mobile_project.LoginFragment
+
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class AuthRepo {
+    private val auth = FirebaseAuth.getInstance()
+
+    fun firebaseEmailSignup(
+        email: String,
+        password: String,
+        user: MutableLiveData<FirebaseUser>,
+        context: Context
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    user.value = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        context, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/programming_mobile_project/LoginFragment/AuthViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.programming_mobile_project.LoginFragment
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseUser
+
+class AuthViewModel(application: Application) : AndroidViewModel(application) {
+    private var authRepository = AuthRepo()
+    private var _authenticatedUser = MutableLiveData<FirebaseUser>()
+    val authenticatedUser: LiveData<FirebaseUser>
+        get() = _authenticatedUser
+
+    fun signUpWithEmail(email: String, password: String) {
+        authRepository.firebaseEmailSignup(email, password, _authenticatedUser, getApplication())
+    }
+}
