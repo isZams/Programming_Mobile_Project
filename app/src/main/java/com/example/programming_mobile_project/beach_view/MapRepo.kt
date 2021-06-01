541e893d75047291118184e67439213d1e9c1aaf
rickardomc <salvatelliriccardo@gmail.com>
2021-06-02 00:41:08 +0200

    può essere prenotato un solo ombrellone alla volta

--- app/src/main/java/com/example/programming_mobile_project/beach_view/MapRepo.kt
+++ app/src/main/java/com/example/programming_mobile_project/beach_view/MapRepo.kt
@@ -53,9 +53,11 @@ class MapRepo {
 
     private fun getJS(): String {
         return "" +
-                "// Funzione invocata al caricamento della pagina\n" +
+                "var ombrello = '';\n" +
+                "    // Funzione invocata al caricamento della pagina\n" +
                 "    function initListener() {\n" +
                 "      const occupati = [1, 4, 6];\n" +
+                "      \n" +
                 "      query = '*[id^=ombr_]';\n" +
                 "      var ombrelli = document.querySelectorAll(query);\n" +
                 "      i = 0;\n" +
@@ -63,19 +65,24 @@ class MapRepo {
                 "      // Agli ombrelloni liberi aggiungo il click listener. \n" +
                 "      // A quelli occupati applico lo stile 'occupato' e non aggiungo il listener\n" +
                 "      for (i = 0; i < ombrelli.length; i++) {\n" +
-                "        if (occupati.includes(i+1)) {\n" +
-                "          ombrelli[i].classList.add('occupato')\n" +
+                "        //se in occupati c'è l'i-esimo ombrello allora questo è occupato\n" +
+                "        if (occupati.includes(i + 1)) {\n" +
+                "          ombrelli[i].classList.add('occupato');\n" +
                 "        }\n" +
                 "        else {\n" +
-                "          ombrelli[i].addEventListener('click', onClickOmbrello)\n" +
+                "          ombrelli[i].addEventListener('click', onClickOmbrello);\n" +
                 "        }\n" +
                 "      }\n" +
                 "    }\n" +
                 "\n" +
                 "    // Applica all'elemento cliccato lo stile 'selected'\n" +
                 "    function onClickOmbrello(e) {\n" +
+                "      if (ombrello !== '') {\n" +
+                "        document.getElementById('ombr_' + ombrello).classList.toggle('selected');\n" +
+                "      }\n" +
+                "      ombrello = e.target.id.split('_')[1];\n" +
                 "      document.getElementById(e.target.id).classList.toggle('selected');\n" +
                 "      Android.clickedOmbrello(e.target.id);\n" +
-                "    }"
+                "    }\n"
     }
 }
