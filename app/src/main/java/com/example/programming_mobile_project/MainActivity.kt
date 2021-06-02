b68c1d99a3c9828c3035938511df76288a43e9e5
rickardomc <salvatelliriccardo@gmail.com>
2021-06-02 19:35:21 +0200

    add toolbar

--- app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
+++ app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.programming_mobile_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
-import com.example.programming_mobile_project.login.AuthFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navHostfragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val nav_controller = navHostfragment.navController
+        val navHostfragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        setSupportActionBar(findViewById<Toolbar>(R.id.main_toolbar))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
