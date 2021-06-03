e6c781dcc5ebbef551ff6e903bd69abebaa2a0c0
Nicolò Rossini <64408085+nicorossini@users.noreply.github.com>
2021-06-03 13:25:04 +0200

    chalet data class

--- app/src/main/java/com/example/programming_mobile_project/chalet_admin/Chalet.kt
+++ app/src/main/java/com/example/programming_mobile_project/chalet_admin/Chalet.kt
@@ -1,7 +1,6 @@
 package com.example.programming_mobile_project.chalet_admin
 
-data class Chalet(val id: Int,
-                  val locandina: String,
+data class Chalet(val locandina: String,
                   val nome_chalet: String,
                   val indirizzo: String,
                   val descrizione: String) {
