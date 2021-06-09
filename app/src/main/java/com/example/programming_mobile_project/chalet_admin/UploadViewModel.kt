3c2b38eca0480ffae703209847dea20189ec7484
Alessandro Muscatello <s1089288@studenti.univpm.it>
2021-06-09 17:29:31 +0200

    UploadFragment.kt UploadViewModel.kt chalet_admin.xml pronti e funzionanti per il caricamento di un nuovo chalet nel database con relativa immagine. Aggiunto metodo dentro chalet db per l'upload dell'immagine

--- app/src/main/java/com/example/programming_mobile_project/chalet_admin/UploadViewModel.kt
+++ app/src/main/java/com/example/programming_mobile_project/chalet_admin/UploadViewModel.kt
@@ -1,52 +1,37 @@
 package com.example.programming_mobile_project.chalet_admin
 
-import android.app.Application
-import android.content.ContentResolver
-import android.content.Context
 import android.net.Uri
-import android.util.Log
-import android.webkit.MimeTypeMap
-import android.widget.Toast
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.programming_mobile_project.database.ChaletDB
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
+import com.example.programming_mobile_project.models.Chalet
+import kotlinx.coroutines.*
 
-
-class UploadViewModel(application: Application) : ViewModel() {
+class UploadViewModel : ViewModel() {
 
     private val uri: Uri = "".toUri()
-    private val context: Context = application.applicationContext
+    private lateinit var image: Uri
 
-    fun upload(){
-        val db: ChaletDB
-       // db.addChalet()
+    /**
+     * Inserisce localmente l'immagine per il successivo invio al database
+     * @param image immagine che viene salvata localmente di tipo Uri
+     */
+    fun storeImage(image: Uri) {
+        this.image = image
     }
-    /*
-    fun downloadUrl(): StorageReference {
-        return storage.getReferenceFromUrl("gs://book-beach.appspot.com/images/Carpa.jpg")
-    }
-    */
-    private fun getFileExtention(uri: Uri): String? {
-        val contentResolver: ContentResolver = context.contentResolver
-        val mimeTypeMap: MimeTypeMap = MimeTypeMap.getSingleton()
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri))
-    }
-    fun uploadImage(){
-        if(uri != null){
-            val storage = Firebase.storage.reference
-            val imageRef = storage.child("images").child(System.currentTimeMillis().toString() + "." + getFileExtention(uri))
-            imageRef.putFile(uri).addOnCompleteListener(){
-                imageRef.downloadUrl.addOnSuccessListener {
-                    val url = uri.toString()
-                    Log.d("downloadUrl", url)
-                    Toast.makeText(context, "image upload success", Toast.LENGTH_SHORT).show()
-                }
-            }
+
+    /**
+     * Funzione che serve al fragment per caricare il nuovo chalet nel database.
+     * Carica in due fasi lo chalet e la sua immagine nello storage sfruttando l'id dello chalet autogenerato da Firebase
+     * @param chalet lo chalet che verrà caricato nel database
+     */
+    fun upload(chalet: Chalet) {
+        viewModelScope.launch {
+            val db = ChaletDB()
+            val id = db.addChalet(chalet)
+            val url = db.uploadImage(image, id)
         }
+        //TODO prendere l'url generato da firebase dell'immagine per metterlo dentro alla locandina
     }
-
-
 }
