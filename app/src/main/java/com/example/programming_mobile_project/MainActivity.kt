aa29f662f8ffd28f8c683055674d9248e846c686
isZams <zamponi.alessandro99@outlook.it>
2021-06-10 12:45:07 +0200

    bottom navigation menu

--- app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
+++ app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.example.programming_mobile_project.Home_Page.HomePage
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,41 +15,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      //  val nav_View = findViewById<BottomNavigationView>(R.id.nav_view)
-        val navHostfragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        setSupportActionBar(findViewById<Toolbar>(R.id.main_toolbar))
+        val bottomNav: BottomNavigationView = findViewById(R.id.nav_view)
 
-   //     val firstFragment = HomePage()
-
-     //   setCurrentFragment(firstFragment)
-
-     /*   nav_View.setOnNavigationItemSelectedListener { item ->
+        bottomNav.setOnNavigationItemSelectedListener OnNavigationItemSelectedListener@{ item ->
             when (item.itemId) {
-                R.id.HomePage -> setCurrentFragment(firstFragment)
+                R.id.item1 -> {
+                    val fragment = HomePage()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    true
+                }
+                R.id.item2 -> {
+                    sendHtmlEmail()
+                    true
+                }
+                else -> false
             }
-            true
-
-        }*/
-    }
-
-
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.HomePage, fragment)
-            commit()
         }
 
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        setSupportActionBar(findViewById<Toolbar>(R.id.main_toolbar))
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
     }
-/*
-    fun sendHtmlEmail(view: View?) {
+
+    fun sendHtmlEmail() {
         val mailId = "Beach.View@gmail.com"
         val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", mailId, null))
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Salve")
         startActivity(Intent.createChooser(emailIntent, "Send email..."))
     }
-*/}
+}
