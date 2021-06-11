58a313a2a1ce96fed5a1eff59e0cb5dc5a5737f2
Nicolò Rossini <64408085+nicorossini@users.noreply.github.com>
2021-06-11 17:53:03 +0200

    MainActivity.kt

--- app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
+++ app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.programming_mobile_project.Home_Page.HomePage
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,15 +18,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bottomNav = findViewById<BottomNavigationView>(R.id.nav_view)
-
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_view)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        setSupportActionBar(findViewById<Toolbar>(R.id.main_toolbar))
         bottomNav.setOnNavigationItemSelectedListener OnNavigationItemSelectedListener@{ item ->
             when (item.itemId) {
                 R.id.item1 -> {
-                    val fragment: Fragment = HomePage()
-                    supportFragmentManager.beginTransaction()
-                       .replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-                        .commit()
+                    val navController = findNavController(R.id.nav_host_fragment)
+                    navController.navigate(R.id.HomePage)
                     true
                 }
                 R.id.item2 -> {
@@ -35,18 +36,13 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        setSupportActionBar(findViewById<Toolbar>(R.id.main_toolbar))
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
     }
-
     fun sendHtmlEmail() {
         val mailId = "Beach.View@gmail.com"
         val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", mailId, null))
@@ -54,3 +50,4 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent.createChooser(emailIntent, "Send email..."))
     }
 }
+
