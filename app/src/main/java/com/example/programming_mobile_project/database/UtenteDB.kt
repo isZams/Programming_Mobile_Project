b0c9459a64f3a4dd21ded0ca5126147d6f96257d
Alessandro Muscatello <s1089288@studenti.univpm.it>
2021-06-11 15:16:13 +0200

    Aggiunta funzione in UtenteDB.kt per aggiungere un utente nel database

--- app/src/main/java/com/example/programming_mobile_project/database/UtenteDB.kt
+++ app/src/main/java/com/example/programming_mobile_project/database/UtenteDB.kt
@@ -20,5 +20,17 @@ class UtenteDB : FirebaseDB() {
             null
         }
     }
+
+    /**
+     * Funzione che prende in input un id ed un Utente e lo inserisce all'interno del database
+     * @param id id dell'utente attuale
+     * @param utente utente da aggiungere al database
+     */
+    suspend fun addUtente(id: String, utente: Utente){
+        try {
+            val response = utenteRef.document(id).set(utente).await()
+        } catch (e: Exception) {
+        }
+    }
 }
 
