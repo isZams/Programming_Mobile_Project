d315f84b7ba295d381486771204fa7608739d23e
rickardomc <salvatelliriccardo@gmail.com>
2021-06-12 17:42:57 +0200

    fix freccia indietro toolbar

--- app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
+++ app/src/main/java/com/example/programming_mobile_project/MainActivity.kt
@@ -5,23 +5,27 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import com.example.programming_mobile_project.Home_Page.HomePage
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_view)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        setSupportActionBar(findViewById<Toolbar>(R.id.main_toolbar))
+        navController = navHostFragment.findNavController()
+        setSupportActionBar(findViewById(R.id.main_toolbar))
+        setupActionBarWithNavController(navController)
+
         bottomNav.setOnNavigationItemSelectedListener OnNavigationItemSelectedListener@{ item ->
             when (item.itemId) {
                 R.id.item1 -> {
@@ -43,6 +47,14 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
     }
+
+    /**
+     * Serve per gestire la freccia indietro in alto a sinistra
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
     fun sendHtmlEmail() {
         val mailId = "Beach.View@gmail.com"
         val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", mailId, null))
